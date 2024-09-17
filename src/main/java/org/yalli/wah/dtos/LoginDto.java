8b3a004e3ddb99960ac2316044393128328c1ca1
emilcahangirli <emil.jahangirli@pashabank.az>
2024-09-17 23:04:14 +0400

    Initial commit

---
+++ src/main/java/org/yalli/wah/dtos/LoginDto.java
@@ -0,0 +1,16 @@
+package org.yalli.wah.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginDto {
+    private String email;
+    private String password;
+
+}
