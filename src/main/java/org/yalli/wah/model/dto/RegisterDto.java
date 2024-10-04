b2921d39fe4bb6191fe5a4b44551f33ac8d5f702
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2024-10-04 15:35:02 +0400

    YALLI-111: validation added

--- src/main/java/org/yalli/wah/model/dto/RegisterDto.java
+++ src/main/java/org/yalli/wah/model/dto/RegisterDto.java
@@ -3,6 +3,7 @@ package org.yalli.wah.model.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +18,13 @@ import java.util.Map;
 public class RegisterDto {
     @NotBlank
     private String fullName;
+    @NotBlank(message = "EMAIL_REQUIRED")
     @Email
     private String email;
-    @NotBlank
+
+    @NotBlank(message = "PASSWORD_REQUIRED")
+    @Pattern(regexp = "^(?=.*[0-9!@#$%^&*])(?=.*[A-Z]).{8,20}$",
+            message = "INVALID_PASSWORD_FORMAT")
     private String password;
     @NotBlank
     private String country;
