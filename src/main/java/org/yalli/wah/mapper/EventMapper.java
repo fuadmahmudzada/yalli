9bb0ccf6c308e84dd63c99cee4aa09599a57cbed
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2024-10-15 18:46:41 +0400

    YALLI-135: Event description

--- src/main/java/org/yalli/wah/mapper/EventMapper.java
+++ src/main/java/org/yalli/wah/mapper/EventMapper.java
@@ -3,6 +3,7 @@ package org.yalli.wah.mapper;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import org.yalli.wah.dao.entity.EventEntity;
+import org.yalli.wah.model.dto.EventDetailDto;
 import org.yalli.wah.model.dto.EventDto;
 
 @Mapper
@@ -10,4 +11,5 @@ public abstract class EventMapper {
     public final static EventMapper INSTANCE = Mappers.getMapper(EventMapper.class);
 
     public abstract EventDto mapEntityToDto(EventEntity event);
+    public abstract EventDetailDto manEntityToEventDetailDto(EventEntity event);
 }
