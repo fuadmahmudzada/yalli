cd798c8f038d6a57416c5b724b8fe3e5ea41cd65
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2024-11-04 22:36:00 +0400

    save events

---
+++ src/main/java/org/yalli/wah/model/dto/EventSaveDto.java
@@ -0,0 +1,13 @@
+package org.yalli.wah.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EventSaveDto {
+    private Long id;
+    private Long userId;
+}
