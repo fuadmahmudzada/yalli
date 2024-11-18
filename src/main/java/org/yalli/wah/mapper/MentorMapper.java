2d7e16eb0af511e7193933c173a2574f9dd4264e
emilcahangirli <emil.jahangirli@pashabank.az>
2024-11-18 04:35:48 +0400

    add mentorship flow

--- src/main/java/org/yalli/wah/mapper/MentorMapper.java
+++ src/main/java/org/yalli/wah/mapper/MentorMapper.java
@@ -7,25 +7,26 @@ import org.yalli.wah.dao.entity.MentorEntity;
 import org.yalli.wah.model.dto.MentorDetailDto;
 import org.yalli.wah.model.dto.MentorSearchDto;
 import org.yalli.wah.model.dto.MentorshipDto;
+import org.yalli.wah.model.enums.MentorStatus;
 
 
-@Mapper
+@Mapper(imports = { MentorStatus.class })
 public interface MentorMapper {
     MentorMapper INSTANCE = Mappers.getMapper(MentorMapper.class);
 
-    @Mapping(target = "fullName", source = "user.fullName")
-    @Mapping(target = "country", source = "user.country")
+    @Mapping(target = "fullName", source = "mentorEntity.user.fullName")
+    @Mapping(target = "country", source = "mentorEntity.user.country")
     MentorDetailDto mapMentorToMentorDetailDto(MentorEntity mentorEntity, Double averageRating);
 
     @Mapping(target = "fullName", source = "user.fullName")
     @Mapping(target = "country", source = "user.country")
     MentorSearchDto mapMentorEntityToMentorSearchDto(MentorEntity mentorEntity);
 
-    @Mapping(target = "status", expression = "java(MentorStatus.APPLIED)")
-    @Mapping(target = "user.id",source = "userId")
-    @Mapping(target = "comments",ignore = true)
-    @Mapping(target = "id",ignore = true)
-    @Mapping(target = "updatedAt",ignore = true)
-    @Mapping(target = "createdAt",ignore = true)
+    @Mapping(target = "status", expression = "java(MentorStatus.valueOf(\"APPLIED\"))")
+    @Mapping(target = "user.id", source = "userId")
+    @Mapping(target = "comments", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
     MentorEntity mapMentorshipDtoToEntity(MentorshipDto mentorshipDto);
 }
