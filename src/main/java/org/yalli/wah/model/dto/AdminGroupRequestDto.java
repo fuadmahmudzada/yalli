9e876902e6c54d41b6e3708e46e44df6a34ac815
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2024-12-04 02:36:12 +0400

    add  admin  group event crud

--- src/main/java/org/yalli/wah/model/dto/AdminGroupRequestDto.java
+++ src/main/java/org/yalli/wah/model/dto/AdminGroupRequestDto.java
@@ -1,2 +1,19 @@
-package org.yalli.wah.model.dto;public class AdminGroupRequestDto {
+package org.yalli.wah.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.yalli.wah.model.enums.GroupCategory;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdminGroupRequestDto {
+    private String title;
+    private String description;
+    private String country;
+    private String imageId;
+    private Long memberCount;
+    private String link;
+    private GroupCategory category;
 }
