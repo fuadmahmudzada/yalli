c53d33fde3d0175c58421927d831acee7769bf94
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2025-01-28 16:10:53 +0400

    change domain

--- src/main/java/org/yalli/wah/config/SecurityConfig.java
+++ src/main/java/org/yalli/wah/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http, ClientRegistrationRepository clientRegistrationRepository) throws Exception {
         CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new CsrfTokenRequestAttributeHandler();
         CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository();
-        csrfTokenRepository.setCookieCustomizer(responseCookieBuilder -> responseCookieBuilder.httpOnly(false).secure(false).sameSite("Lax").domain("localhost").domain("yalli-front-end.vercel.app").domain("yalli.org"));
+        csrfTokenRepository.setCookieCustomizer(responseCookieBuilder -> responseCookieBuilder.httpOnly(false).secure(false).sameSite("None"));
 
         http.authorizeHttpRequests((requests) -> requests
                                 .requestMatchers(HttpMethod.GET, "/v1/admins").hasAnyRole("ADMIN", "SUPER_ADMIN", "MODERATOR")
