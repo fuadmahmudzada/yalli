be86ee0dddb5475b44db77e544a61112e7f11ac0
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2025-02-08 13:20:47 +0400

    add cors

--- src/main/java/org/yalli/wah/config/SecurityConfig.java
+++ src/main/java/org/yalli/wah/config/SecurityConfig.java
@@ -121,7 +121,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+                        config.setAllowedOrigins((Collections.singletonList("https://yalli-org.vercel.app")));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
