f8411a038c9f7f368fab5c54b38f205542240e99
fuadmahmudzada <fuadmahnudzadabooming@gmail.com>
2025-02-22 22:04:29 +0400

    change csrf generation

--- src/main/java/org/yalli/wah/filter/CsrfCookieFilter.java
+++ src/main/java/org/yalli/wah/filter/CsrfCookieFilter.java
@@ -4,6 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -14,32 +16,32 @@ import java.util.Map;
 
 public class CsrfCookieFilter extends OncePerRequestFilter {
     private final List<String> securedEndpoints = List.of(
-            "/v1/admins",
-            "/v1/admins/{adminId}",
-            "/v1/admins/events/**",
-            "/v1/admins/add-notification",
-            "/v1/admins/create-group",
-            "/v1/admins/delete-group",
-            "/v1/admins/update-group/",
-            "/v1/users/delete/{id}",
-            "/v1/events/saveEvent",
-            "/v1/events/unsaveEvent",
-            "/v1/files/upload",
-            "/v1/groups",
-            "/v1/groups/users/{userId}",
-            "/v1/groups",
-            "/v1/mentors",
-            "/v1/mentors/{id}"
-            ,"/v1/users/login"
+//            "/v1/admins",
+//            "/v1/admins/{adminId}",
+//            "/v1/admins/events/**",
+//            "/v1/admins/add-notification",
+//            "/v1/admins/create-group",
+//            "/v1/admins/delete-group",
+//            "/v1/admins/update-group/",
+//            "/v1/users/delete/{id}",
+//            "/v1/events/saveEvent",
+//            "/v1/events/unsaveEvent",
+//            "/v1/files/upload",
+//            "/v1/groups",
+//            "/v1/groups/users/{userId}",
+//            "/v1/groups",
+//            "/v1/mentors",
+//            "/v1/mentors/{id}"
+            "/v1/users/login"
     );
 
     private final AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         boolean isSecuredApi = securedEndpoints.stream().anyMatch(api -> antPathMatcher.match(api, request.getRequestURI())) && !request.getMethod().equals("GET");
-        if (isSecuredApi) {
+        if (isSecuredApi && (authentication!=null || authentication.equals("anonymousUser"))) {
             CsrfToken csrfToken = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
             if (csrfToken != null) {
                 csrfToken.getToken();
